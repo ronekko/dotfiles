0885cfee3bb6ded1bfa2fd3c11ae94aaf5088ce0
Ryuhei Sakurai <rsakurai@fc.ritsumei.ac.jp>
2016-12-08 13:50:37 +0900

    Fix target diectory

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.vim ~/.vim
+ln -sf ~/dotfiles/.bashrc ~/
+ln -sf ~/dotfiles/.zshrc ~/
+ln -sf ~/dotfiles/.vimrc ~/
+ln -sf ~/dotfiles/.vim ~/
 
